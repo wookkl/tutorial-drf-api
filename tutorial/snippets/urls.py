eb0184762bd779ab748f9dfba955f53d33766f46
wookkl <wjddnr315@gmail.com>
2021-01-22 18:06:58 +0900

    Tutorial 2: Requests and responses

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 
+from rest_framework.urlpatterns import format_suffix_patterns
+
 from snippets import views
 
 app_name = 'snippets'
@@ -8,3 +10,5 @@ urlpatterns = [
     path(r'', views.snippet_list, name='snippets-list'),
     path(r'<int:pk>/', views.snippet_detail, name='snippets-detail'),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
